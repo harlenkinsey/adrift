d1035b7f0e69d8c4bcc8a86dd793f418f715ac62
Harlen Kinsey <harlenkinsey60@gmail.com>
2024-03-25 11:45:30 -0500

    item / inventory updates

---
+++ Main/Player/UI/Inventory/ItemSlot/ItemSlot.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class ItemSlot : Panel
+{
+	public TextureRect IconTextureRect;
+	public TextureRect RarityTextureRect;
+	public Label CountLabel;
+	public ProgressBar DurabilityProgressBar;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		IconTextureRect = (TextureRect)FindChild("IconTextureRect");
+		RarityTextureRect = (TextureRect)FindChild("RarityTextureRect");
+		CountLabel = (Label)FindChild("CountLabel");
+		DurabilityProgressBar = (ProgressBar)FindChild("DurabilityProgressBar");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
